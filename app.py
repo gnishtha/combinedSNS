c059ddf282cac8680abbe97d2d8530c6260fd30a
gnishtha <nishtha.gupta_ccv17@gla.ac.in>
2021-01-26 14:06:41 +0530

    commit

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
-from flask import render_template, Flask, request
+from flask import Flask, request, Response
+from flask_api import status
 
 app = Flask(__name__)
 
 from combinedSNS import preprocessing
 
-@app.route('/predict',  methods=['POST', 'GET'])
+@app.route('/api/predict/',  methods=['POST', 'GET'])
 def predict():
     prediction = ''
     if request.method == 'POST':
-        comment = request.form['comment']
+        data = request.get_json(force = True)
+        comment = data.get('comment','')
+        if comment=='':
+            return "Empty Comment", status.HTTP_400_BAD_REQUEST
         prediction = preprocessing(comment)
-    return render_template('end.html', prediction=prediction)
-
-
-@app.route('/')
-def index():
-    return render_template('prediction.html')
+    return prediction
 
 
 if __name__ == '__main__':
--- combinedSNS.py
+++ combinedSNS.py
@@ -1,48 +1,50 @@
-from tensorflow.keras.models import load_model
 import numpy as np
-from janome.tokenizer import Tokenizer
+import pandas as pd
+import requests
 from janome.analyzer import Analyzer
 from janome.charfilter import *
 from janome.tokenfilter import *
-from keras.preprocessing.text import Tokenizer as tok
+from janome.tokenizer import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-import pandas as pd
-import requests
-import MeCab
+from keras.preprocessing.text import Tokenizer as kerastokenizer
+from tensorflow.keras.models import load_model
 
 url = 'http://svn.sourceforge.jp/svnroot/slothlib/CSharp/Version1/SlothLib/NLP/Filter/StopWord/word/Japanese.txt'
 response = requests.get(url)
 stopwords = [w for w in response.content.decode().split('\r\n') if w != '']
 
-# tagger = MeCab.Tagger('/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/unidic_lite/dicdir/mecabrc')
-# target_parts_of_speech = ('名詞')
-
 char_filters = [UnicodeNormalizeCharFilter(), RegexReplaceCharFilter(u'蛇の目', u'janome')]
 tokenizer = Tokenizer()
-token_filters = [CompoundNounFilter(), POSStopFilter(['記号','助詞']), LowerCaseFilter()]
+token_filters = [CompoundNounFilter(), POSStopFilter(['記号', '助詞']), LowerCaseFilter()]
 a = Analyzer(char_filters=char_filters, tokenizer=tokenizer, token_filters=token_filters)
 
+
 def cleantext(text):
-  text=' '.join(text)
-  return re.sub(' +', ' ', re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', " ", text).replace('https://', ''))
+    text = ' '.join(text)
+    return re.sub(' +', ' ', re.sub(
+        r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s(
+        )<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''',
+        " ", text).replace('https://', ''))
+
 
 def fetch_tokenize(text):
     tokenized_text = []
     for chunk in [token.surface for token in a.analyze(text)]:
-      if chunk not in stopwords:
-          tokenized_text.append(chunk.lower())
+        if chunk not in stopwords:
+            tokenized_text.append(chunk.lower())
     return cleantext(' '.join(tokenized_text))
+
 
 def preprocessing(text):
     dict = {}
     dict['tokenized_comment'] = [fetch_tokenize(text)]
-    X = pd.DataFrame(dict)
-    tokenizer = tok(split=' ')
-    tokenizer.fit_on_texts(X)
-    X = tokenizer.texts_to_sequences(X)
-    X = pad_sequences(X,maxlen= 1301)
-    predic = prediction(X)
-    return predic
+    df = pd.DataFrame(dict)
+    tokenizer = kerastokenizer(split=' ')
+    tokenizer.fit_on_texts(df)
+    df = tokenizer.texts_to_sequences(df)
+    df = pad_sequences(df, maxlen=1301)
+    predict = prediction(df)
+    return predict
 
 
 def prediction(X_test):
@@ -50,16 +52,16 @@ def prediction(X_test):
     # Y_test = np.array(pd.read_csv('Y_test.csv'))
     prediction = ''
     for i in X_test:
-        w, q = compopinion(i)
-        print(q)
+        w, q = complain(i)
+        # print(q)
         if q == 1:
             prediction = 'Opinion'
         elif q == 0:
-            ar, lab = cbncb(i)
-            #print(max(ar[0]), lab)
-            if max(ar[0])>0.80 and lab==0:
+            ar, lab = bug(i)
+            # print(max(ar[0]), lab)
+            if max(ar[0]) > 0.80 and lab == 0:
                 prediction = 'Critical Bug'
-            elif max(ar[0])>0.80 and lab==1:
+            elif max(ar[0]) > 0.80 and lab == 1:
                 prediction = 'Non-Critical Bug'
             else:
                 prediction = 'Complain'
@@ -67,20 +69,20 @@ def prediction(X_test):
     return prediction
 
 
-def compopinion(i):
+def complain(i):
     model = load_model("complainopinion")
     w = model.predict(np.array([i, ]))
     q = model.predict_classes(np.array([i, ]))
     return w, q
 
-def cbncb(i):
+
+def bug(i):
     model = load_model("cbncb")
     w = model.predict(np.array([i, ]))
     q = model.predict_classes(np.array([i, ]))
     return w, q
 
-
 # complain->0, opiniom->1
-#1-.[0,1], 0-[1,0]
-#cb-0, ncb-1
-#preprocessing('あたり　今月最後かも')
+# 1-.[0,1], 0-[1,0]
+# cb-0, ncb-1
+# preprocessing('あたり　今月最後かも')
